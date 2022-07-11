ad5b0183f993ff1e127c7b7cfda6b67352215343
杨文俊 <1091261998@qq.com>
2022-07-11 22:58:25 +0800

    test

--- chapter2/test.go
+++
@@ -1,7 +0,0 @@
-package chapter2
-
-import "fmt"
-
-func Test() {
-    fmt.Println("hello")
-}
--- chapter2/test1.go
+++
@@ -1,9 +0,0 @@
-package chapter2
-
-import (
-    "fmt"
-)
-
-func test2 () {
-    fmt.Println("hello")
-}
